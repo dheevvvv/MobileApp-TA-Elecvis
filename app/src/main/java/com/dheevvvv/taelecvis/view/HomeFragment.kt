a66a03431843ad62267512a0c2dabef0246979f1
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-11-14 01:51:01 +0700

    adding bottom menu

--- app/src/main/java/com/dheevvvv/taelecvis/view/HomeFragment.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/view/HomeFragment.kt
@@ -5,18 +5,26 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.dheevvvv.taelecvis.R
+import com.dheevvvv.taelecvis.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
-
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        
     }
 
 
