a19b6e5a29b1e453cbc71bea085e88c0bf15a339
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-11-27 14:03:03 +0700

    update profileFragment

--- app/src/main/java/com/dheevvvv/taelecvis/view/ProfileFragment.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/view/ProfileFragment.kt
@@ -5,14 +5,28 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.navigation.fragment.findNavController
 import com.dheevvvv.taelecvis.R
 import com.dheevvvv.taelecvis.databinding.FragmentProfileBinding
+import com.dheevvvv.taelecvis.datastore_preferences.UserManager
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.async
 
 
 class ProfileFragment : Fragment() {
     private lateinit var binding: FragmentProfileBinding
+    private lateinit var userManager: UserManager
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
 
+    private val auth by lazy {
+        FirebaseAuth.getInstance()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -51,6 +65,39 @@ class ProfileFragment : Fragment() {
             }
         }
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient = GoogleSignIn.getClient(requireActivity(), gso)
+
+        binding.clLogout.setOnClickListener {
+            showLogoutConfirmationDialog()
+        }
+
+    }
+
+    private fun showLogoutConfirmationDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(requireContext())
+        alertDialogBuilder.setTitle("Konfirmasi Logout")
+        alertDialogBuilder.setMessage("Apakah Anda yakin ingin keluar?")
+        alertDialogBuilder.setPositiveButton("Ya") { _, _ ->
+            performLogout()
+        }
+        alertDialogBuilder.setNegativeButton("Tidak") { dialog, _ ->
+            dialog.dismiss()
+        }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
+    }
+
+    private fun performLogout() {
+        mGoogleSignInClient.signOut()
+        GlobalScope.async {
+            userManager.clearData()
+        }
+        Toast.makeText(requireContext(), "Logout Berhasil, Anda telah Logout", Toast.LENGTH_SHORT).show()
+        findNavController().navigate(R.id.action_profileFragment_to_splashFragment)
     }
 
 
