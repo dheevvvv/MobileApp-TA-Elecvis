048e3aa1239199b6236d67415d3617fec1447d16
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-11-27 13:18:28 +0700

    update userviewmodel

--- app/src/main/java/com/dheevvvv/taelecvis/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/viewmodel/UserViewModel.kt
@@ -1,4 +1,25 @@
 package com.dheevvvv.taelecvis.viewmodel
 
-class UserViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.dheevvvv.taelecvis.datastore_preferences.UserManager
+import kotlinx.coroutines.launch
+
+class UserViewModel(val userManager: UserManager):ViewModel() {
+
+    private val _username = MutableLiveData<String>()
+    val username: LiveData<String> = _username
+
+    private val _email = MutableLiveData<String>()
+    val email: LiveData<String> = _email
+
+
+    fun getUsername() {
+        viewModelScope.launch {
+            val username = userManager.getUsername()
+            _username.postValue(username)
+        }
+    }
 }
