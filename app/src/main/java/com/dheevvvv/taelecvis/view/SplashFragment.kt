0050317e05e1101c81eea619fdc3e2b0fed4829d
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-11-27 13:49:08 +0700

    update splashFragment

--- app/src/main/java/com/dheevvvv/taelecvis/view/SplashFragment.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/view/SplashFragment.kt
@@ -10,14 +10,18 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
 import android.view.animation.TranslateAnimation
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.dheevvvv.taelecvis.R
 import com.dheevvvv.taelecvis.databinding.FragmentSplashBinding
+import com.dheevvvv.taelecvis.datastore_preferences.UserManager
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 
 class SplashFragment : Fragment() {
     private lateinit var binding:FragmentSplashBinding
-
+    private lateinit var userManager: UserManager
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -30,12 +34,19 @@ class SplashFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        userManager = UserManager.getInstance(requireContext())
         val fadeInAnimation = AnimationUtils.loadAnimation(requireContext(), R.anim.anim_splash)
         binding.ivLogoSplash.startAnimation(fadeInAnimation)
 
         Handler().postDelayed({
-            findNavController().navigate(R.id.action_splashFragment_to_welcomePageFragment)
+            lifecycleScope.launch {
+                if (userManager.isLoggedIn().first()){
+                    findNavController().navigate(R.id.action_splashFragment_to_homeFragment)
+                }else{
+                    userManager.clearData()
+                    findNavController().navigate(R.id.action_splashFragment_to_welcomePageFragment)
+                }
+            }
         }, 3000)
     }
 
