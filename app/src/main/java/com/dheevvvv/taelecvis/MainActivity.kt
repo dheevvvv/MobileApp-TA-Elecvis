71bba1fb3a6d59690a6210ef1c9dfa113fde9393
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2023-11-27 13:45:18 +0700

    update mainActivity

--- app/src/main/java/com/dheevvvv/taelecvis/MainActivity.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/MainActivity.kt
@@ -2,10 +2,47 @@ package com.dheevvvv.taelecvis
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.window.OnBackInvokedDispatcher
+import androidx.appcompat.app.AlertDialog
+import androidx.core.os.postDelayed
+import androidx.lifecycle.lifecycleScope
+import com.dheevvvv.taelecvis.datastore_preferences.UserManager
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var userManager: UserManager
+    private var doubleBackToExitPressedOnce = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        userManager = UserManager.getInstance(this)
+
+    }
+
+    @Suppress("DEPRECATION")
+    override fun onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed()
+        } else {
+            showExitConfirmationDialog()
+        }
+    }
+
+    private fun showExitConfirmationDialog() {
+        val alertDialogBuilder = AlertDialog.Builder(this)
+        alertDialogBuilder.setMessage("Tekan kembali lagi untuk keluar")
+        val alertDialog = alertDialogBuilder.create()
+
+        alertDialog.show()
+
+        Handler(Looper.getMainLooper()).postDelayed(2000) {
+            alertDialog.dismiss()
+            doubleBackToExitPressedOnce = false
+        }
+
+        doubleBackToExitPressedOnce = true
     }
 }
