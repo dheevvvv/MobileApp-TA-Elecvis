87dd8f326d2669c7eae82a1e554e1e8e9907cf79
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2024-02-09 04:13:37 +0700

    update UserViewModel adding login and register

--- app/src/main/java/com/dheevvvv/taelecvis/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/dheevvvv/taelecvis/viewmodel/UserViewModel.kt
@@ -1,13 +1,21 @@
 package com.dheevvvv.taelecvis.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.dheevvvv.taelecvis.datastore_preferences.UserManager
+import com.dheevvvv.taelecvis.model.UserPostLoginRequest
+import com.dheevvvv.taelecvis.model.UserPostRequest
+import com.dheevvvv.taelecvis.model.UserPostResponse
+import com.dheevvvv.taelecvis.networking.ApiService
 import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-class UserViewModel(val userManager: UserManager):ViewModel() {
+class UserViewModel(private val userManager: UserManager, private val apiService: ApiService):ViewModel() {
 
     private val _username = MutableLiveData<String>()
     val username: LiveData<String> = _username
@@ -15,11 +23,63 @@ class UserViewModel(val userManager: UserManager):ViewModel() {
     private val _email = MutableLiveData<String>()
     val email: LiveData<String> = _email
 
+    private val _loginUsers: MutableLiveData<UserPostResponse?> = MutableLiveData()
+    val loginUsers: MutableLiveData<UserPostResponse?> get() = _loginUsers
+
+    private val _registerUser: MutableLiveData<UserPostResponse?> = MutableLiveData()
+    val registerUser: MutableLiveData<UserPostResponse?> get() = _registerUser
+
 
     fun getUsername() {
         viewModelScope.launch {
             val username = userManager.getUsername()
             _username.postValue(username)
         }
+    }
+
+    private fun callApiUserPostLogin(email:String, password:String){
+        apiService.loginUser(UserPostLoginRequest(email, password)).enqueue(object :
+            Callback<UserPostResponse>{
+            override fun onResponse(
+                call: Call<UserPostResponse>,
+                response: Response<UserPostResponse>
+            ) {
+                if (response.isSuccessful){
+                    val data = response.body()
+                    _loginUsers.postValue(data!!)
+                } else{
+                    _loginUsers.postValue(null)
+                    Log.e("Error:", "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<UserPostResponse>, t: Throwable) {
+                _loginUsers.postValue(null)
+                Log.e("Error:", "onFailure: ${t.message}")
+            }
+        })
+    }
+
+    private fun callApiUserPostRegister(email: String, password: String, name:String, username:String, phoneNumber:String){
+        apiService.registerUser(UserPostRequest(email, password, name, username, phoneNumber)).enqueue(object : Callback<UserPostResponse>{
+            override fun onResponse(
+                call: Call<UserPostResponse>,
+                response: Response<UserPostResponse>
+            ) {
+                if (response.isSuccessful){
+                    val data = response.body()
+                    _registerUser.postValue(data!!)
+                } else{
+                    _registerUser.postValue(null)
+                    Log.e("Error:", "onFailure: ${response.message()}")
+                }
+            }
+
+            override fun onFailure(call: Call<UserPostResponse>, t: Throwable) {
+                _registerUser.postValue(null)
+                Log.e("Error:", "onFailure: ${t.message}")
+            }
+
+        })
     }
 }
