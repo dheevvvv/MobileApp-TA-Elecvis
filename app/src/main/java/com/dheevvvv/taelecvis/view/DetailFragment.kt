151b2b72ae31c84b2c0b29a62020e11eebc97d28
dhevadayatvitoindraj <dhevadayatvitoindraj@mail.ugm.ac.id>
2024-03-05 12:36:57 +0700

    adding detail fragment

---
+++ app/src/main/java/com/dheevvvv/taelecvis/view/DetailFragment.kt
@@ -0,0 +1,43 @@
+package com.dheevvvv.taelecvis.view
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.dheevvvv.taelecvis.R
+import com.dheevvvv.taelecvis.databinding.FragmentDetailBinding
+import com.dheevvvv.taelecvis.view.adapter.DetailAdapter
+import com.google.android.material.tabs.TabLayoutMediator
+
+
+class DetailFragment : Fragment() {
+    private lateinit var binding: FragmentDetailBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentDetailBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val tabLayout = binding.tabLayoutDetail
+        val viewPager = binding.viewPagerDetail
+        val tabTitlesList = listOf("Description Info", "Recommendation")
+
+        val fragments = listOf(ElectricInfoFragment(), RecomendationFragment())
+        val adapter = DetailAdapter(childFragmentManager, fragments, tabTitlesList)
+        viewPager.adapter = adapter
+
+
+        tabLayout.setupWithViewPager(viewPager)
+    }
+
+
+}
---
+++ app/src/main/java/com/dheevvvv/taelecvis/view/adapter/DetailAdapter.kt
@@ -0,0 +1,15 @@
+package com.dheevvvv.taelecvis.view.adapter
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+class DetailAdapter (fm: FragmentManager, private val fragments: List<Fragment>, private val titles: List<String>) :
+    FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+
+    override fun getCount(): Int = fragments.size
+
+    override fun getItem(position: Int): Fragment = fragments[position]
+
+    override fun getPageTitle(position: Int): CharSequence? = titles[position]
+}
